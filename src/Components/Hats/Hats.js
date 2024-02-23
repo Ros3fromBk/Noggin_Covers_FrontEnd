1a9097674c44c627cdc22260b40ad140db3bac31
Rose <i.a.rose.bk@gmail.com>
2024-02-23 14:25:15 -0500

    first commit

---
+++ src/Components/Hats/Hats.js
@@ -0,0 +1,17 @@
+import "../Hats/Hats.css"
+import axios from "axios";
+import {useEfffect,useState} from "react";
+
+
+export default function Hats() {
+  
+    return (
+
+
+    <div className="Hats">
+        <section className="Hats-title">
+      <h1>Noggin' Covers</h1>
+        </section>
+    </div>
+  );
+}
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <div className="Navbar" >
+
+      <section className="Nav-main">
+      <h3> Welcome to Noggin' Covers</h3>
+      </section>
+
+      <section className="Nav-body">
+      <p> Dad Hats </p>
+      <p> Buckets </p>
+      <p> Beanies </p>
+      <p> SURPRISE ME </p>
+
+
+      </section>
+    </div>
+  );
+}
